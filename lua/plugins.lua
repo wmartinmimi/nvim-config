3338a71188b397a7fe9b8f6ac1c0bfe259000085
wmartinmimi <wmartinmimipub@gmail.com>
2024-02-22 23:46:00 +0000

    update test key bindings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -231,7 +231,7 @@ require('lazy').setup({
 
       -- required by spell
       vim.opt.spell = true
-      vim.opt.spelllang = { 'en_uk', 'en_us' }
+      vim.opt.spelllang = { 'en_us' }
 
 
       local cmp = require('cmp')
@@ -268,45 +268,20 @@ require('lazy').setup({
           }
         },
         mapping = {
-          ['<Down>'] = map(map.select_next_item(), {'i', 's', 'c'}),
-          ['<Up>'] = map(map.select_prev_item(), {'i', 's', 'c'}),
-          ['<M-Left>'] = map(map.abort(), {'i', 's', 'c'}),
-          ["<Tab>"] = map(function(fallback)
+          ['<M-[>'] = map(map.select_prev_item(), {'i', 's', 'c'}),
+          ['<M-]>'] = map(map.select_next_item(), {'i', 's', 'c'}),
+          ['<M-Enter>'] = map(map.abort(), {'i', 's', 'c'}),
+          ["<Enter>"] = map(function(fallback)
             -- enter selected completion
-            -- enter 1st completion if none selected
             if cmp.visible() then
               local entry = cmp.get_selected_entry()
-              if not entry then
-                cmp.select_next_item({
-                  behavior = cmp.SelectBehavior.Select
-                })
-
-              else
+              if entry then
                 cmp.confirm()
               end
             else
               fallback()
             end
           end, {'i','s','c'}),
-          ["<CR>"] = map({
-            i = function(fallback)
-              if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({
-                  behavior = cmp.ConfirmBehavior.Replace,
-                  select = false
-                })
-              else
-                fallback()
-              end
-            end,
-            s = map.confirm({
-              select = true
-            }),
-            c = map.confirm({
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true
-            }),
-          })
         },
         sources = cmp.config.sources({
           { name = 'calc' },
@@ -338,14 +313,6 @@ require('lazy').setup({
         sources = {
           { name = 'buffer' }
         }
-      })
-      cmp.setup.cmdline(':', {
-        mapping = map.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'async_path' }
-        }, {
-            { name = 'cmdline' }
-          })
       })
     end
   },
