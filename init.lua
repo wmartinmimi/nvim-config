8b51ea6185f6a01ad111fff146bb6b29d7d234c6
wmartinmimi <wmartinmimipub@gmail.com>
2026-01-13 00:15:40 +0000

    fix cmdline not working as expected

--- init.lua
+++ init.lua
@@ -426,6 +426,10 @@ local config = {
       vim.opt.spelllang = { 'en_gb', 'en_us' }
     end,
     opts = {
+      cmdline = {
+        keymap = { preset = 'inherit' },
+        completion = { menu = { auto_show = true } },
+      },
       completion = {
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
         ghost_text = { enabled = true },
